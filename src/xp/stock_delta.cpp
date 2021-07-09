cfc0f3de5a77b1dfdf1bd77f550f4adfd8bf2098
Jichen Li <jli218@stevens.edu>
2021-07-09 21:01:56 +0800

    Update stock_delta.cpp
    Add function nextday and isTodayClose. When a day is closed fill everything with '0'

--- src/xp/stock_delta.cpp
+++ src/xp/stock_delta.cpp
@@ -23,6 +23,7 @@ class Stock {
   int16_t volume_approximate;
   bool empty_stock : 1;
   bool first_day : 1;
+  uint16_t year, month, day;
 
  public:
   Stock()
@@ -42,7 +43,8 @@ class Stock {
   // Remove negative bit
   friend ostream& operator<<(ostream& o, const Stock& t) {
     if (t.first_day) {
-      return o << setprecision(5) << t.open_price << " " << t.high_delta << " "
+      return o << t.year << "-" << t.month << "-" << t.day << " "
+               << setprecision(5) << t.open_price << " " << t.high_delta << " "
                << t.low_delta << " " << t.close_delta << " "
                << t.volume_approximate << '\n';
     } else {
@@ -89,28 +91,72 @@ class Stock {
     vol_log = ceil(log10(abs(vol_delta))) - 3;
     volume_approximate = (int64_t(vol_delta / pow(10, vol_log)) << 4) + vol_log;
   };
-  static bool approxEqual(float a, float b, float eps) {
-    return abs(b - a) < eps;
-  };
-  static bool reportError(const Stock& s1, const Stock& s2, float eps1,
-                          float eps2) {
-    bool ok = true;
-    if (!(ok &= approxEqual(s1.open_price, s2.open_price, eps1))) {
-      std::cerr << "error:" << s1.open_price << " " << s2.open_price;
-    };
-    if (!(ok &= approxEqual(s1.high, s2.high, eps1))) {
-      std::cerr << "error:" << s1.high << " " << s2.high;
-    };
-    if (!(ok &= approxEqual(s1.low, s2.low, eps1))) {
-      std::cerr << "error:" << s1.low << " " << s2.low;
-    };
-    if (!(ok &= approxEqual(s1.close, s2.close, eps1))) {
-      std::cerr << "error:" << s1.close << " " << s2.close;
-    };
-    if (!(ok &= approxEqual(s1.volume, s2.volume, eps2))) {
-      std::cerr << "error:" << s1.volume << " " << s2.volume;
-    };
-  };
+  void tradingSuspended() {
+    open_delta = 0;
+    high_delta = 0;
+    low_delta = 0;
+    close_delta = 0;
+    volume_approximate = 0;
+  }
+  Stock nextDay(Stock* s) {
+    int newDay = s->day + 1;
+    int newMonth = s->month;
+    int newYear = s->year;
+    if (s->month == 1 || s->month == 3 || s->month == 5 || s->month == 7 ||
+        s->month == 8 || s->month == 10 || s->month == 12) {
+      if (s->day = 31) {
+        if (s->month = 12) {
+          newDay = 1;
+          newMonth = 1;
+          newYear = s->year + 1;
+        }
+      }
+    } else if (s->month == 4 || s->month == 6 || s->month == 9 ||
+               s->month == 11) {
+      if (s->day = 30) {
+        newDay = 1;
+        newMonth = s->month + 1;
+      }
+    } else if (s->month == 2) {
+      if (s->year % 4 == 0) {
+        if (s->year % 100 == 0) {
+          if (s->year % 400 == 0) {
+            if (s->day == 29) {
+              newDay = 1;
+              newMonth = s->month + 1;
+            }
+          }
+        }
+      } else {
+        if (s->day == 28) {
+          newDay = 1;
+          newMonth = s->month + 1;
+        }
+      }
+    }
+    day = newDay;
+    month = newMonth;
+    year = newYear;
+  }
+  int day_of_week(int year,int month, int day) {
+    
+  }
+  bool isTodayClose() {
+    int copy_year = year;
+    int copy_month = month;
+    if (copy_month == 1 || copy_month == 2) {
+      copy_month += 12;
+      copy_year--;
+    }
+    int week = (day + 2 * copy_month + 3 * (copy_month + 1) / 5 + copy_year +
+                copy_year / 4 - copy_year / 100 + copy_year / 400 + 1) %
+               7;
+    if (week == 6 || week == 0) {
+      return true;
+    }
+
+  }
+
   float get_close_price() { return close; }
   bool isEmpty() { return empty_stock; }
   uint64_t get_vol() { return volume; }
@@ -139,14 +185,16 @@ class Compress_stock {
     char buf[1024];
     f.getline(buf, sizeof(buf));
     Stock s;
+    Stock empty;
+    empty.tradingSuspended();
 
     while (f >> s) {
       if (stocks.size() > 0)
         s.delta(&stocks[stocks.size() - 1]);
       else
         s.delta();
-      stocks.push_back(s);
     }
+    stocks.push_back(s);
   };
   friend ostream& operator<<(ostream& o, const Compress_stock& c) {
     for (int i = 0; i < c.stocks.size(); i++) {
